f6cf3693216e26e12478091e3bea09f851c6322a
aayushrai <www.aayushrai@gmail.com>
2020-05-13 19:33:09 +0530

    Django

--- MyFirstSite/music/views.py
+++ MyFirstSite/music/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from .models import Album,Song
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>This music app Homepage</h1>")
+    all_album = Album.objects.all()
+    html = ""
+    for album in all_album:
+        url = "/music/" + str(album.id) + "/"
+        html += '<a href=' + url + '>' + album.album_title + '</a><br>'
+    return HttpResponse(html)
+
+def details(request,album_id):
+    return HttpResponse("<h1>Album Id:" + str(album_id) + "</h1>")
