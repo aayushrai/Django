e0698e672de58c0d5836a7385f61838ad1fbaff2
aayushrai <www.aayushrai@gmail.com>
2020-05-15 17:48:40 +0530

    Favorite

--- MyFirstSite/music/urls.py
+++ MyFirstSite/music/urls.py
@@ -5,4 +5,5 @@ app_name = "music"
 urlpatterns = [
     path('',views.index,name='index'),
     path('<int:album_id>/',views.details,name='details'),
+    path('<int:album_id>/favorite/',views.favorite,name='favorite'),
 ]
--- MyFirstSite/music/views.py
+++ MyFirstSite/music/views.py
@@ -11,3 +11,14 @@ def details(request,album_id):
    # album = Album.objects.get(pk=album_id)
     album = get_object_or_404(Album, pk=album_id)
     return render(request,"music/details.html",{"album":album})
+
+def favorite(request,album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        song = album.song_set.get(pk=request.POST["song"])
+    except (KeyError,Song.DoesNotExist):
+        return render(request, "music/details.html", {"album": album,"error:message":"You did not select valid song"})
+    else:
+        song.is_favorite =True
+        song.save()
+        return render(request, "music/details.html", {"album": album})
