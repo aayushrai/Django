b05b409909ccb63e3192c0fcccf25572f139634a
aayushrai <www.aayushrai@gmail.com>
2020-05-24 21:08:37 +0530

    ip camera working

--- VideoStream/streamapp/views.py
+++ VideoStream/streamapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
-from .camera import VideoCamera, IPWebCam
+from .camera import VideoCamera
 # Create your views here.
 
 def index(request):
@@ -12,11 +12,12 @@ def gen(camera):
 		yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 def video_feed(request):
-	return StreamingHttpResponse(gen(VideoCamera()),content_type='multipart/x-mixed-replace; boundary=frame')
+	return StreamingHttpResponse(gen(VideoCamera(0)),content_type='multipart/x-mixed-replace; boundary=frame')
 
 def webcam_feed(request):
+	#link = "http://192.168.43.1:8080/video"
 	link = str(request.POST["link"])
-	return StreamingHttpResponse(gen(IPWebCam(link)),content_type='multipart/x-mixed-replace; boundary=frame')
+	return StreamingHttpResponse(gen(VideoCamera(link)),content_type='multipart/x-mixed-replace; boundary=frame')
 
 def ipweblink(request):
 	return render(request,"streamapp/ipwebcam.html")
