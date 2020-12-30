04c8f7023a418dff98fa1e4d3b2a7cfe8154cada
aayushrai <www.aayushrai@gmail.com>
2020-12-30 23:02:40 +0530

    multiple camera flask server

---
+++ Face_recognition_on_flask_Server_multiple_camera /client.py
@@ -0,0 +1,9 @@
+import requests
+import cv2
+import json
+
+url = "http://127.0.0.1:5000/stop"
+ip_cam1 = "http://192.168.52.81:8080/video"
+ip_cam = 0
+# x = requests.post(url, data = {"ip_cam":ip_cam1})
+y = requests.post(url, data = {"ip_cam":ip_cam})
